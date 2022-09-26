15fbef35d6963d0e41e21bb8cca6296d7817e286
afarangurens <afarangurens@unal.edu.co>
2022-09-26 18:11:52 -0500

    Added more models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,5 @@
+from tkinter import CASCADE
+from typing import Dict
 from django.conf import settings
 from django.db import models  
 
@@ -18,33 +20,55 @@ class Client(models.Model):
     def __str__(self):
         return "%S %S" % (self.name, self.surname)
 
-class Product(models.Model):
-    name = models.CharField(max_length=50, default="")
-    price = models.FloatField(default=0.0)
-    specifications = models.CharField(max_length=200, default="")
-    
-    def __str__(self):
-        return "%S"  % (self.name)
-
 class ProductCategory(models.Model):
     name = models.CharField(max_length=50, default="")
     description = models.CharField(max_length=200, default="")
 
+    def __str__(self):
+        return "%S" % (self.name)
+
+class Discount(models.Model):
+    name = models.CharField(max_length=50, default="")
+    description = models.CharField(max_length=200, default="")
+    discount_percentage = models.FloatField(default=0.0)
+    active = models.BooleanField(default=True)
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, default=True, null=False)
+    name = models.CharField(max_length=50,  default=True, null=False)
+    price = models.DecimalField(max_digits=15, decimal_places=0,  default=True, null=False)
+    specifications = models.CharField(max_length=200,  default=True, null=False)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%S" % (self.name)
+
+    def get_discount_price(self):
+        return self.price * self.discount.discount_percentage
 
 class ProductInventory(models.Model):
-    in_stock = models.PositiveIntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=True, null=False)
+    quantity = models.PositiveIntegerField(default=0)
 
 class ProductOnQuotation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                             on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=True, null=False)
+    quantity = models.PositiveIntegerField(null=False, default=0)
     
     def __str__(self):
         return f"{self.quantity} of {self.product.name}"
 
     def get_total_item_price(self):
-        return self.quantity * self.product.price 
+        return self.quantity * self.product.price
+    
+    def get_total_discount_item_price(self):
+        return self.quantity * self.product.get_discount_price
+
+    def get_total_price(self):
+        if self.product.discount.active:
+            return self.get_total_item_price()
+        return self.get_total_discount_item_price()
 
 class Quotation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -55,9 +79,9 @@ class Quotation(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.cliente.name
     
-    def total_amount(self):
+    def get_total_amount(self):
         total = 0
         for quotated_product in self.products.all():
             total += quotated_product.get_total_item_price()
@@ -74,11 +98,7 @@ class Receipt(models.Model):
 class ProductOnReceipt(models.Model):
     pass
 
-class Discount(models.Model):
-    pass
 
-class User(models.Model):
-    pass
 
 
 
