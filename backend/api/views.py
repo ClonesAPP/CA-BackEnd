c40c14272b8f7a6336567b027cd14380103f18f1
afarangurens <afarangurens@unal.edu.co>
2022-10-19 18:22:46 -0500

    Test login done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,28 @@
-from tkinter.font import ROMAN
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Quotation
-from .forms import QuotationForm    
 
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponse
+
+from backend.settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
+from .models import Quotation, ProductOnQuotation, Product
+from .forms import QuotationForm
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.shortcuts import redirect
 
 # Create your views here.
+@login_required(redirect_field_name=LOGOUT_REDIRECT_URL)
 def home(request):
     quotations = Quotation.objects.all()
     context = {'quotations': quotations}
     return render(request, 'api/home.html', context)
 
+@login_required(redirect_field_name=LOGOUT_REDIRECT_URL)
 def quotation(request, pk):
     quotation = Quotation.objects.get(id=pk)
     context = {'quotation': quotation}
     return render(request, 'api/quotation.html', context)
 
+@login_required(redirect_field_name=LOGOUT_REDIRECT_URL)
 def create_quotation(request):
     form = QuotationForm()
     
@@ -27,4 +34,3 @@ def create_quotation(request):
             
     context = {'form': form}
     return render(request, 'api/create_quotation.html', context)
-
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,3 +137,4 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "login"
